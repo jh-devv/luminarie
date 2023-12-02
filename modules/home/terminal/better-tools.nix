97f668856a25fd541b258146a6ecd50e0b4cd9f8
jh-devv <122896463+jh-devv@users.noreply.github.com>
2023-12-02 16:10:39 +0200

    feat: grep to rga

--- modules/home/terminal/better-tools.nix
+++ modules/home/terminal/better-tools.nix
@@ -3,13 +3,15 @@ let
   cfg = config.jh-devv.home.terminal.better-tools;
 in {
   config = lib.mkIf cfg.enable { 
-    home.packages = with pkgs; [
+    home.packages = with pkgs; [r
+        rga
         fd
         lsd
     ];
     home.shellAliases = {
         ls = "lsd";
         find = "fd --glob";
+        grep = "rga";
     };
     };
 }
