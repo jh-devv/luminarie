50bec5052503afcfbb96a78e00cb1450e8462e17
jh-devv <122896463+jh-devv@users.noreply.github.com>
2023-12-30 17:39:46 +0200

    feat: add flatpak override config

--- modules/home/rice/gtk/default.nix
+++ modules/home/rice/gtk/default.nix
@@ -28,8 +28,16 @@ in {
         name = "Catppuccin-Mocha-Lavender-Cursors";
     };
 
-    # For flatpaks, kinda gross but works uwu
+    # This could probably be done with XDG theme directories uwu
     home.file.".themes/Catppuccin-Mocha-Standard-Lavender-Dark".source = "${pkgs.catppuccin-gtk.override { accents = ["lavender"]; variant = "mocha"; }}/share/themes/Catppuccin-Mocha-Standard-Lavender-Dark";
     home.file.".icons/Papirus-Dark".source = "${pkgs.catppuccin-papirus-folders}/share/icons/Papirus-Dark/";
+    
+    xdg.dataFile."flatpak/overrides/global".source = (pkgs.formats.ini {}).generate "global" {
+      Context.filesystems = "~/.icons:ro;~/.themes:ro;/nix/store/:ro";
+      Environment = {
+        GTK_THEME = "Catppuccin-Mocha-Standard-Lavender-Dark";
+        ICON_THEME = "Papirus-Dark";
+      };
+    };
   };
 }
