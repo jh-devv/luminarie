90636bd82dd0bdffdc0513b8f26a3b5d894e6411
jh-devv <122896463+jh-devv@users.noreply.github.com>
2024-01-01 16:09:58 +0200

    fix: hibernate

--- hosts/luminara/hardware-configuration.nix
+++ hosts/luminara/hardware-configuration.nix
@@ -32,7 +32,11 @@
       fsType = "vfat";
     };
 
-  swapDevices = [ ];
+  swapDevices = [ {
+      device = "/var/lib/swapfile";
+      size = 32*1024;
+    } ];
+
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
