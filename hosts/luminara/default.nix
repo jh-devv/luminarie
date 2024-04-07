4f87bc97039b1038bca0a02522c9f2fbc82ff812
jh-devv <jh-devv@jh-devv.com>
2024-04-07 16:46:31 +0300

    chore(hosts): cleaner file names

--- hosts/luminara/default.nix
+++ hosts/luminara/default.nix
@@ -1,5 +1,26 @@
-{...}: {
+{pkgs, ...}: {
   imports = [
-    ./configuration.nix
+    ./hardware.nix
   ];
+
+  modules.nixos = {
+    shell = "zsh";
+  };
+
+  networking = {
+    hostName = "luminara";
+    networkmanager.enable = true;
+  };
+
+  users.users.jh-devv = {
+    shell = pkgs.zsh;
+    isNormalUser = true;
+    extraGroups = ["wheel" "docker"];
+  };
+
+  time.timeZone = "Europe/Helsinki";
+
+  services.xserver.layout = "fi";
+
+  system.stateVersion = "23.05";
 }
